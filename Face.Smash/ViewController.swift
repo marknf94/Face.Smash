936352c66e0f977c2007c161d5a555e9be6278e2
Mark Fernandez <marknf@uci.edu>
2017-01-14 04:29:27 -0800

    Initial Commit

---
+++ Face.Smash/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Face.Smash
+//
+//  Created by Mark Fernandez on 1/14/17.
+//  Copyright © 2017 Mark Fernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
